5e6d3f1cefd6b4f737c67710455dbb758d13f06d
Viktor <komvik.slack@gmail.com>
2024-09-05 22:41:23 +0200

    Start of the program setting the task

---
+++ app_play_basketball/bin/main.dart
@@ -0,0 +1,9 @@
+void main() {
+// user register or login.(login/logout/delete user)
+// have info about player.(How many players play on courts and wich corts.)
+// inform at what time and where it will play.
+// post a message to the group.
+
+  print('\n________  Start _____________\n');
+//,
+}
---
+++ app_play_basketball/test/app_play_basketball_test.dart
@@ -0,0 +1,8 @@
+import 'package:app_play_basketball/app_play_basketball.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
